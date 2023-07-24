ddaa8a7c7c8ab1bd49a11e637a697762d0092fca
FenixFVE <49178401+FenixFVE@users.noreply.github.com>
2023-07-24 07:48:23 +0000

    Climbing Stairs

--- Solutions/ClimbingStairs.cs
+++ Solutions/ClimbingStairs.cs
@@ -3,8 +3,24 @@ namespace Solutions;
 
 public partial class Solution
 {
+
     public int ClimbStairs(int n) {
-        throw new NotImplementedException();
+
+        if (n == 0 || n == 1) 
+            return 1;
+
+        var prev1 = 1;
+        var prev2 = 1;
+        var result = 0;
+
+        for (var i = 2; i <= n; i++) 
+        {
+            result = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = result;
+        }
+
+        return result;
     }
 
     public static bool TestClimbStairs() {
